401e4f769fd7a642db46c9f61d3ff842488e3be1
wstrinz <wstrinz@gmail.com>
2013-05-31 21:31:10 -0500

    Revert "fixed?"
    This reverts commit 4c7f8b600818421793b0ded22fa1179cac428d97.

--- lib/speech/audio_inspector.rb
+++ lib/speech/audio_inspector.rb
@@ -48,13 +48,12 @@ module Speech
 
     def initialize(file)
       out = `ffmpeg -i #{file} 2>&1`.strip
-      # puts "out: #{out}"
-      # if out.match(/No such file or directory/)
-      #   raise "No such file or directory: #{file}"
-      # else
+      if out.match(/No such file or directory/)
+        raise "No such file or directory: #{file}"
+      else
         out = out.scan(/Duration: (.*),/)
         self.duration = Duration.new(out.first.first)
-      # end
+      end
     end
 
   end
